b36ab1ab8a4ccf9e7dc5adfbc4179c2cdec15997
Huiming <610416981@qq.com>
2024-08-14 22:12:31 +0800

    :construction: building: add lsm & cache

---
+++ dataServer/router/api/v1/data.go
@@ -0,0 +1,19 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func WriteData(c *gin.Context) {
+	// Write data to the database
+}
+
+func ReadData(c *gin.Context) {
+	// Read data from the database
+}
+
+func UpdateData(c *gin.Context) {
+	// Update data in the database
+}
+
+func DeleteData(c *gin.Context) {
+	// Delete data from the database
+}
